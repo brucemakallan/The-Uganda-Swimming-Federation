438ee22be51dc594b48947e95bdb5725a06ba7a6
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-06-04 14:03:10 +0300

    Article pagination on the Homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './home.scss';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import ReactPaginate from 'react-paginate';
 import Carousel from '../Carousel';
 import ArticleCard from '../ArticleCard';
 import FeaturedVideos from '../FeaturedVideos';
@@ -12,79 +13,125 @@ const addBackgroundImage = url => ({
   backgroundImage: `url(${url})`,
 });
 
-const Home = ({
-  carousel, about, articles, events,
-}) => (
-  <div className="mainContent">
-    <PageLoader />
+const ARTICLES_PER_PAGE = 3;
 
-    <section id="banner">
-      <Carousel id="main" imageUrls={carousel.imageUrls} captions={carousel.captions} />
-    </section>
+class Home extends Component {
+  state = {
+    pagination: {
+      start: 0,
+      end: ARTICLES_PER_PAGE,
+    },
+  }
 
-    <section className="articles large-padding responsive-flex">
-      {articles && articles.length > 0
-      && (
-        <div className="left responsive-flex-child inner-padding">
-          {articles.map(article => <ArticleCard key={article._id} article={article} />)}
-        </div>
-      )}
+  onPageChange = ({ selected }) => {
+    const start = selected * ARTICLES_PER_PAGE;
+    const end = start + ARTICLES_PER_PAGE;
+    this.setState({
+      pagination: {
+        start,
+        end,
+      },
+    });
+  }
 
-      {events && events.length > 0 && (
-        <div className="right responsive-flex-child inner-padding">
-          <div>Sidebar Content here</div>
-          <ul>
-            <li>Results</li>
-            <li>Events</li>
-            <li>Social Feed</li>
-          </ul>
-        </div>
-      )}
-    </section>
+  render() {
+    const {
+      carousel, about, articles, events
+    } = this.props;
+    const { pagination: { start, end } } = this.state;
+    const paginatedArticles = articles.slice(start, end);
 
-    {about && about.length > 0
-    && about[0].images && about[0].images.length > 0
-    && about[0].body && about[0].body.length > 0
-    && (
-      <section className="about" style={addBackgroundImage(about[0].images[0])}>
-        <div className="inner-padding about-info">
-          <ReactMarkdown source={about[0].body} />
-        </div>
-      </section>
-    )}
+    return (
+      <div className="mainContent">
+        <PageLoader />
 
-    {articles && articles.length > 0 && (
-      <React.Fragment>
-        <section className="photos section-padding">
-          <div className="inner-padding">
-            <h3>Featured Photos</h3>
-            <FeaturedPhotos articles={articles} />
+        <section id="banner">
+          <Carousel id="main" imageUrls={carousel.imageUrls} captions={carousel.captions} />
+        </section>
+
+        <section className="articles large-padding responsive-flex">
+          {(paginatedArticles && paginatedArticles.length > 0)
+            ? (
+              <div className="left responsive-flex-child inner-padding">
+                {paginatedArticles.map(article => <ArticleCard key={article._id} article={article} />)}
+                <ReactPaginate
+                  previousLabel="PREVIOUS"
+                  nextLabel="NEXT"
+                  breakLabel="..."
+                  breakClassName="break-me"
+                  pageCount={Math.ceil(articles.length / ARTICLES_PER_PAGE)}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={this.onPageChange}
+                  containerClassName="pagination"
+                  subContainerClassName="pages pagination"
+                  activeClassName="active"
+                />
+              </div>
+            ) : (
+              <span>
+                <span role="img" aria-label="sad">😢</span>
+                No Articles
+              </span>
+            )}
+
+          {events && events.length > 0 && (
+            <div className="right responsive-flex-child inner-padding">
+              <div>Sidebar Content here</div>
+              <ul>
+                <li>Results</li>
+                <li>Events</li>
+                <li>Social Feed</li>
+              </ul>
+            </div>
+          )}
+        </section>
+
+        {about && about.length > 0
+        && about[0].images && about[0].images.length > 0
+        && about[0].body && about[0].body.length > 0
+        && (
+          <section className="about" style={addBackgroundImage(about[0].images[0])}>
+            <div className="inner-padding about-info">
+              <ReactMarkdown source={about[0].body} />
+            </div>
+          </section>
+        )}
+
+        {articles && articles.length > 0 && (
+          <React.Fragment>
+            <section className="photos section-padding">
+              <div className="inner-padding">
+                <h3>Featured Photos</h3>
+                <FeaturedPhotos articles={articles} />
+              </div>
+            </section>
+            <section className="videos section-padding">
+              <div className="inner-padding">
+                <h3>Featured Videos</h3>
+                <FeaturedVideos articles={articles} />
+              </div>
+            </section>
+          </React.Fragment>
+        )}
+
+        <section className="footer dark">
+          <div className="responsive-flex">
+            <div className="responsive-flex-child third section-padding">
+              footer1
+            </div>
+            <div className="responsive-flex-child third section-padding">
+              footer2
+            </div>
+            <div className="responsive-flex-child third section-padding">
+              footer3
+            </div>
           </div>
         </section>
-        <section className="videos section-padding">
-          <div className="inner-padding">
-            <h3>Featured Videos</h3>
-            <FeaturedVideos articles={articles} />
-          </div>
-        </section>
-      </React.Fragment>
-    )}
-
-    <section className="footer dark">
-      <div className="responsive-flex">
-        <div className="responsive-flex-child third section-padding">
-          footer1
-        </div>
-        <div className="responsive-flex-child third section-padding">
-          footer2
-        </div>
-        <div className="responsive-flex-child third section-padding">
-          footer3
-        </div>
       </div>
-    </section>
-  </div>
-);
+    );
+  }
+}
 
 Home.propTypes = {
   carousel: PropTypes.shape({}).isRequired,
