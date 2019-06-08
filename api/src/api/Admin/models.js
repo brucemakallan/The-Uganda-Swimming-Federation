e7a6f431dcda970f43956ef66afe0573f1d8802b
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-06-08 13:08:55 +0300

    Remove API folder from frontend

--- api/src/api/Admin/models.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('../databaseConnection');
-const schema = require('./schema');
-
-const Schema = mongoose.Schema;
-const fields = new Schema({ ...schema });
-const model = mongoose.model('Admin', fields);
-module.exports = model;
--- api/src/api/Article/models.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('../databaseConnection');
-const schema = require('./schema');
-
-const Schema = mongoose.Schema;
-const fields = new Schema({ ...schema });
-const model = mongoose.model('Article', fields);
-module.exports = model;
