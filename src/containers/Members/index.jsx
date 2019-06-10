757bea49bcdf501e179790db7cbda13fa99d324b
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-06-10 18:27:30 +0300

    Render list of USF Members

--- src/containers/Members/index.jsx
+++ src/containers/Members/index.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ReactMarkdown from 'react-markdown';
 import getAllProducts from '../../actions/productsActions';
-import { pageSections, endpoints, freshCoatOfPaint } from '../../utils';
+import { pageSections, endpoints } from '../../utils';
+import './members.scss';
 
 
 class Members extends Component {
@@ -14,9 +16,29 @@ class Members extends Component {
   render() {
     const { products } = this.props;
     const membersList = products.filter(section => section.category === pageSections.membersPage);
-    // console.log(membersList);
 
-    return freshCoatOfPaint();
+    return (
+      <div className="mainContent">
+        <div className="members-page">
+          <div className="large-padding responsive-flex">
+            <h1 className="sub-section-heading">
+              USF Members
+              <hr />
+            </h1>
+            {membersList && membersList.length > 0 && (
+              <div className="responsive-flex-child inner-padding full">
+                {membersList.map(category => (
+                  <div key={category.heading1} className="material-card mb-5">
+                    <h3 className="category-heading">{category.heading1}</h3>
+                    <ReactMarkdown source={category.body} />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
